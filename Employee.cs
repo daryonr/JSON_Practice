e170f40809d0ef6f729935afc434a199be4f1040
Daryon Roshanzaer <108088780+daryonr@users.noreply.github.com>
2023-03-02 19:00:53 -0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace UpdatedProject.Models
+{
+    public class Employee
+    {
+        public string? EmployeeID { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Number { get; set; }
+    }
+
+}
